a02171c506da5ea2230cf3b716257477d4824c9a
whybecauseican <70674401+whybecauseican@users.noreply.github.com>
2024-09-24 21:12:31 +0800

    Update NFT.js

--- NFT.js
+++ NFT.js
@@ -1,18 +1,25 @@
 // Create a variable to hold your NFTs
 const NFTs = [];
 
+// Function to generate a random transaction ID
+function generateTransactionID() {
+    return 'TX-' + Math.random().toString(36).substr(2, 9); // Random string
+}
+
 // This function will take in values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
-// and store it in the NFTs array.
+// and store it in the NFTs array, along with a transaction ID.
 function mintNFT(_name, _eyeColor, _shirtType, _bling) {
+    const transactionID = generateTransactionID();
     const NFT = {
         "Name": _name,
         "EyeColor": _eyeColor,
         "ShirtType": _shirtType,
-        "Bling": _bling
+        "Bling": _bling,
+        "TransactionID": transactionID // Add transaction ID to the NFT object
     };
     NFTs.push(NFT);
-    console.log("Minted: " + _name);
+    console.log("Minted: " + _name + " | Transaction ID: " + transactionID);
 }
 
 // Create a "loop" that will go through the NFTs array
@@ -24,6 +31,7 @@ function listNFTs() {
         console.log("  Eye Color: " + NFTs[i].EyeColor);
         console.log("  Shirt Type: " + NFTs[i].ShirtType);
         console.log("  Bling: " + NFTs[i].Bling);
+        console.log("  Transaction ID: " + NFTs[i].TransactionID); // Output transaction ID
     }
 }
 
